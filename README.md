d109e0ce307c14ce837f5baf853e5d44d2da2e7f
Rinaldi <rinaldiamfine@gmail.com>
2024-01-02 06:52:16 +0700

    Add the documentation for database migrations

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# bevent-backend
+# Bevent
+
+## Migrations
+Create migration files
+```
+npx sequelize-cli migration:generate --name {filename}
+```
+Running the migration
+```
+npx sequelize-cli db:migrate --env production
+```
+or (revert back the migrations)
+```
+npx sequelize-cli db:migrate:undo --env production
+```
--- migrations/20240101100416-events.js
+++ migrations/20240101100416-events.js
@@ -4,9 +4,23 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.createTable(
-      'events', { 
-        id: Sequelize.INTEGER 
-      }
+      'events', 
+      {
+        id: Sequelize.INTEGER,
+        name: Sequelize.STRING,
+        created_by: Sequelize.INTEGER,
+        created_at: {
+          type: Sequelize.DATE,
+          defaultValue: new Date(),
+          allowNull: false
+        },
+        updated_by: Sequelize.INTEGER,
+        updated_at: {
+          type: Sequelize.DATE,
+          defaultValue: new Date(),
+          allowNull: false
+        },
+      },
     );
   },
 
